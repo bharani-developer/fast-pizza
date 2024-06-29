aaf484e8cfa4edd3f257c60a281d665cb1f59668
Bharani karthikeyan <bharani.developer@gmail.com>
2024-06-29 18:29:35 +0530

    Layout completed

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <div>
+      <Header>
+        <Link to="/">Fast Pizza</Link>
+      </Header>
+    </div>
+  );
+}
+
+export default Header
